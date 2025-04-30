c49023def5881a6a8fe04b628825974010059274
reklamingo <arifkurtulmuslar@gmail.com>
2025-05-01 02:56:04 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use('/api/images', require('./routes/images'));
 
 // MongoDB bağlantısı ve sunucunun ayağa kalkması
 mongoose.connect(process.env.MONGO_URI)
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   .then(() => {
     app.listen(PORT, () => {
       console.log('Sunucu çalışıyor: ' + PORT);
