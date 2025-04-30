6b1db050cc39a637e07c09b7b8aaeb075f9f2870
reklamingo <arifkurtulmuslar@gmail.com>
2025-05-01 01:33:08 +0300

    Update Image.js

--- backend/models/Image.js
+++ backend/models/Image.js
@@ -1,1 +1,8 @@
-// görsel modeli
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  filename: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Image', imageSchema);
