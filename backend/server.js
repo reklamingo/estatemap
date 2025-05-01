4c4d0165ece1e58fb39eb8cb2762db13e6533292
reklamingo <arifkurtulmuslar@gmail.com>
2025-05-01 21:50:41 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-// Rotalar
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/parsel', require('./routes/parsel'));
 app.use('/api/images', require('./routes/images'));
 
-// MongoDB bağlantısı ve sunucunun ayağa kalkması
 mongoose.connect(process.env.MONGO_URI)
   useNewUrlParser: true,
   useUnifiedTopology: true,
