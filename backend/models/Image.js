098d8a9863db231b9f8cb7398db1797d58713c72
reklamingo <arifkurtulmuslar@gmail.com>
2025-05-02 00:58:18 +0300

    Delete backend directory

--- backend/models/Image.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  filename: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('Image', imageSchema);
